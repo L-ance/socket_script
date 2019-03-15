d70c05dac7da8ffc8f662bde672860aaf4926b19
L-ance <42347605+L-ance@users.noreply.github.com>
2019-03-15 16:45:37 +0800

    socket server

--- socket_recv.py
+++ socket_recv.py
@@ -4,33 +4,41 @@ import struct
 import threading
 import time
 import sys
+import json
 
 # HOST = '192.168.2.68'
 # PORT = 60000
-BUFFER_SIZE = 1024
+BUFFER_SIZE = 1024 * 1024 * 60
 HEAD_STRUCT = 'q'  # Structure of file head
 lst = 50
 success_num = 0  # connect success number
 error_num = 0  # connect error number
 moment_recv_size = 0  # 2s accept data
 all_recv_size = 0  # all success data
-e = ''
 mu = threading.Lock()
-
+TIME_PRINT = 1
+Transmitting = 0
 
 def jonnyS(client_socket):
-    global e
+    global Transmitting
     global BUFFER_SIZE
     global success_num
     global error_num
     global HEAD_STRUCT
     global all_recv_size
     global moment_recv_size
+    mu.acquire()
+    Transmitting += 1
+    mu.release()
     recv_size = 0
     info_struct = struct.calcsize(HEAD_STRUCT)
-    file_info = client_socket.recv(info_struct)
-    data_size = struct.unpack(HEAD_STRUCT, file_info)[0]
-    # data_size: type(tuple)
+
+    while True:
+        file_info = client_socket.recv(info_struct)
+        if len(file_info) == 8:
+            data_size = struct.unpack(HEAD_STRUCT, file_info)[0]
+            break
+        # data_size: type(tuple)
     try:
         while recv_size < data_size:
             if data_size - recv_size < BUFFER_SIZE:
@@ -38,13 +46,17 @@ def jonnyS(client_socket):
             else:
                 file_data = client_socket.recv(BUFFER_SIZE)
             recv_size += len(file_data)
+            mu.acquire()
             moment_recv_size += len(file_data)
+            mu.release()
             if file_data < 0 or file_data == 0:
+                mu.acquire()
                 all_recv_size += data_size
+                mu.release()
                 break
         if recv_size == data_size:
-            all_recv_size += data_size
             mu.acquire()
+            all_recv_size += data_size
             success_num += 1
             mu.release()
         else:
@@ -53,10 +65,13 @@ def jonnyS(client_socket):
             mu.release()
     except Exception as e:
         mu.acquire()
+        print e
         error_num += 1
         mu.release()
     client_socket.close()
-
+    mu.acquire()
+    Transmitting -= 1
+    mu.release()
 
 def recv_file(args):
     _, HOST, PORT = args
@@ -75,21 +90,44 @@ def monitor():
     global error_num
     global all_recv_size
     global moment_recv_size
-    global e
+    global TIME_PRINT
+    global Transmitting
+    s_num = success_num
+    e_num = error_num
+    a_r_s = all_recv_size
+    m_r_s = moment_recv_size
     time.sleep(2)
-    print "Pressed Ctrl+C flush data"
-    print "success_sum: %s" % success_num
-    print "error_num: %s" % error_num
-    print "each_recv_size: %s" % moment_recv_size
-    print "all_recv_size: %s" % all_recv_size
-    print "error message: %s" % e
-    print "-" * 30
-    moment_recv_size = 0
+    dic = {"success_num": success_num, "error_num": error_num, "all_recv_size": all_recv_size,
+           "moment_recv_size": moment_recv_size, "online_threading":Transmitting,"time": time.strftime("%Y-%m-%d %H-%M-%S")}
+    f.write(json.dumps(dic))
+    f.write('\n')
+    if success_num == s_num and error_num == e_num and all_recv_size == a_r_s and moment_recv_size == m_r_s:
+        if TIME_PRINT:
+            # print "The last time {}".format(time.strftime("%Y-%m-%d %H-%M-%S"))
+            mu.acquire()
+            TIME_PRINT = 0
+            mu.release()
+    else:
+        print "{}".format(time.strftime("%Y-%m-%d %H-%M-%S"))
+        print "Pressed Ctrl+C flush data"
+        print "success_sum: %s" % success_num
+        print "error_num: %s" % error_num
+        print "each_recv_size: %s" % moment_recv_size
+        print "all_recv_size: %s" % all_recv_size
+        print "online_threading: %s" % Transmitting
+        print "-" * 30
+
+        moment_recv_size = 0
+        if not TIME_PRINT:
+            mu.acquire()
+            TIME_PRINT = 1
+            mu.release()
 
 
 if __name__ == '__main__':
     recv_file_thread = threading.Thread(target=recv_file, args=(sys.argv,))
     recv_file_thread.start()
+    f = open('log', 'w+')
     while True:
         try:
             monitor()
